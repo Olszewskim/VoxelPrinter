8ca0b4699860f9ddf67393660a80bbaf6e7b8b83
Olszewskim <olszewskim1991@gmail.com>
2020-10-21 20:37:58 +0200

    Show border on current printed layer

--- Assets/Scripts/Core/VoxelElement.cs
+++ Assets/Scripts/Core/VoxelElement.cs
@@ -1,20 +1,32 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VoxelElement : MonoBehaviour {
     private static readonly Vector3 _hideScale = new Vector3(0, 0, 0);
     private static readonly Vector3 _beforePrintScale = new Vector3(1, 0.01f, 1);
     private static readonly Vector3 _showScale = new Vector3(1, 1, 1);
 
+    [SerializeField] private Image _frame;
+
     public void Hide() {
         transform.localScale = _hideScale;
+        _frame.gameObject.SetActive(false);
     }
 
     public void Print(float time) {
-        transform.DOScale(_showScale, time).SetEase(Ease.Linear);
+        _frame.color = Color.white;
+        _frame.DOColor(Color.clear, .4f).SetLoops(-1);
+        transform.DOScale(_showScale, time).SetEase(Ease.Linear)
+            .OnComplete(OnElementPrinted);
+    }
+
+    private void OnElementPrinted() {
+        _frame.gameObject.SetActive(false);
     }
 
     public void PrepareToPrint() {
         transform.localScale = _beforePrintScale;
+        _frame.gameObject.SetActive(true);
     }
 }
