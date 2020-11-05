9a581f16c9a501919da556199642e22d96deeccd
Olszewskim <olszewskim1991@gmail.com>
2020-11-05 21:35:35 +0100

    Feature - animated percentage

--- Assets/Scripts/UI/LevelCompletedWindow.cs
+++ Assets/Scripts/UI/LevelCompletedWindow.cs
@@ -1,11 +1,20 @@
 ﻿using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelCompletedWindow : WindowBehaviour<LevelCompletedWindow> {
+    private const float PERCENTAGE_ANIM_TIME = 2f;
+
     [SerializeField] private TextMeshProUGUI _percentText;
 
     public void ShowWindow(float stageFinishedAtPercentage) {
-        _percentText.text = $"{(int)(stageFinishedAtPercentage * 100)}%";
+        var finalPercent = stageFinishedAtPercentage * 100;
+        _percentText.text = "0%";
+        DOVirtual.Float(0, finalPercent, PERCENTAGE_ANIM_TIME, AnimatePercentage).SetEase(Ease.OutCubic);
         ShowWindow();
+    }
+
+    private void AnimatePercentage(float value) {
+        _percentText.text = $"{(int) value}%";
     }
 }
