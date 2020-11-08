437737b9c8a2b020e30beff897ef1fc027820829
Olszewskim <olszewskim1991@gmail.com>
2020-11-08 18:38:24 +0100

    Feature - add voxel figures data

---
+++ Assets/Scripts/Data/VoxelFigureData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using static Enums;
+
+[CreateAssetMenu(fileName = "VoxelFigureData", menuName = "Data/VoxelFigureData")]
+public class VoxelFigureData : ScriptableObject {
+    public string figureID;
+    public CollectionType collectionType;
+    public string figureName;
+    public VoxelFigure voxelFigure;
+}
---
+++ Assets/Scripts/Utils/Enums.cs
@@ -0,0 +1,5 @@
+﻿public static class Enums {
+    public enum CollectionType {
+        Animals
+    }
+}
