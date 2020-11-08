97162b41733437b236869a91bd336d6597b3ebfc
Olszewskim <olszewskim1991@gmail.com>
2020-11-08 20:02:24 +0100

    Feature - print all models from list

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,14 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Enums;
 
-public class GameManager : MonoBehaviour {
-    [SerializeField] private VoxelFigure _voxelFigure;
+public class GameManager : Singleton<GameManager> {
+    [SerializeField] private List<VoxelFigureData> _voxelFiguresData;
     [SerializeField] private Printer _printer;
+
+    private Dictionary<CollectionType, List<VoxelFigureData>> _voxelFiguresDataDictionary;
+    private CollectionType _currentCollection = CollectionType.Animals;
+    private int _currentFigureID;
+    private VoxelFigure _currentVoxelFigure;
 
     public void Start() {
         Input.multiTouchEnabled = false;
         Vibration.Init();
-        _printer.SetupPrintModel(_voxelFigure);
+        InitVoxelFiguresDictionary();
+        InitNewFigure();
+    }
+
+    private void InitVoxelFiguresDictionary() {
+        _voxelFiguresDataDictionary = new Dictionary<CollectionType, List<VoxelFigureData>>();
+        foreach (var voxelData in _voxelFiguresData) {
+            if (!_voxelFiguresDataDictionary.ContainsKey(voxelData.collectionType)) {
+                _voxelFiguresDataDictionary[voxelData.collectionType] = new List<VoxelFigureData>();
+            }
+
+            _voxelFiguresDataDictionary[voxelData.collectionType].Add(voxelData);
+        }
+    }
+
+    private void InitNewFigure() {
+        if (_currentVoxelFigure != null) {
+            Destroy(_currentVoxelFigure.gameObject);
+        }
+
+        _currentVoxelFigure = Instantiate(_voxelFiguresData[_currentFigureID].voxelFigure);
+        _printer.SetupPrintModel(_currentVoxelFigure);
+    }
+
+    public void LoadNextFigure() {
+        _currentFigureID = Mathf.Min(_currentFigureID + 1, _voxelFiguresDataDictionary[_currentCollection].Count - 1);
+        InitNewFigure();
+    }
+
+    public string GetCurrentVoxelFigureName() {
+        return _voxelFiguresDataDictionary[_currentCollection][_currentFigureID].figureName;
     }
 
     public void ResetGame() {
--- Assets/Scripts/UI/LevelCompletedWindow.cs
+++ Assets/Scripts/UI/LevelCompletedWindow.cs
@@ -1,23 +1,37 @@
 ﻿using TMPro;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class LevelCompletedWindow : WindowBehaviour<LevelCompletedWindow> {
     private const float PERCENTAGE_ANIM_TIME = 2f;
 
     [SerializeField] private TextMeshProUGUI _percentText;
+    [SerializeField] private TextMeshProUGUI _completedText;
     [SerializeField] private StarsControllerUI _starsControllerUI;
+    [SerializeField] private Button _nextFigureButton;
+
+    protected override void Start() {
+        base.Start();
+        _nextFigureButton.onClick.AddListener(PrintNextFigure);
+    }
 
     public void ShowWindow(float stageFinishedAtPercentage) {
         _starsControllerUI.HideStars();
         var finalPercent = stageFinishedAtPercentage * 100;
         _starsControllerUI.ShowStars(finalPercent);
         _percentText.text = "0%";
+        _completedText.text = $"{GameManager.Instance.GetCurrentVoxelFigureName()} Completed";
         DOVirtual.Float(0, finalPercent, PERCENTAGE_ANIM_TIME, AnimatePercentage).SetEase(Ease.OutCubic);
         ShowWindow();
     }
 
     private void AnimatePercentage(float value) {
         _percentText.text = $"{(int) value}%";
+    }
+
+    private void PrintNextFigure() {
+        CloseWindow();
+        GameManager.Instance.LoadNextFigure();
     }
 }
