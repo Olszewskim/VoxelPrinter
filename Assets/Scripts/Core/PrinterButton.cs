95b10981c60fe8323269023b3b70bd157b1623f0
Olszewskim <olszewskim1991@gmail.com>
2020-11-26 22:12:40 +0100

    Enhancement - bigger printer buttons. Bake light maps

--- Assets/Scripts/Core/PrinterButton.cs
+++ Assets/Scripts/Core/PrinterButton.cs
@@ -12,13 +12,16 @@ public class PrinterButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     private const float BUTTON_Y_ANIM_SCALE = 0.5f;
     private const float BUTTON_ANIM_TIME = 0.5f;
 
+
+    private float _startScaleY;
     private MeshRenderer _meshRenderer;
     private bool _isClicked;
     private Color _myColor;
 
     private void Awake() {
         _meshRenderer = _buttonTop.GetComponent<MeshRenderer>();
-        _buttonTop.DOScaleY(1, 0);
+        _startScaleY = transform.localScale.y;
+        _buttonTop.DOScaleY(_startScaleY, 0);
     }
 
     public void SetButtonColor(Color color) {
@@ -36,7 +39,7 @@ public class PrinterButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     public void OnPointerUp(PointerEventData eventData) {
         _isClicked = false;
         _buttonTop.DOKill();
-        _buttonTop.DOScaleY(1, BUTTON_ANIM_TIME);
+        _buttonTop.DOScaleY(_startScaleY, BUTTON_ANIM_TIME);
         OnButtonReleased?.Invoke();
     }
 }
